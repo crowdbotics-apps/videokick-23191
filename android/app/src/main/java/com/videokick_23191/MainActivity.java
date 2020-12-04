38266fd6faa0c3570b220234ba32eae1c677a7d2
Crowdbotics <team@crowdbotics.com>
2020-12-04 14:10:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/videokick_23191/MainActivity.java
@@ -0,0 +1,15 @@
+package com.videokick_23191;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "videokick_23191";
+    }
+}
